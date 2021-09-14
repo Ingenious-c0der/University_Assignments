702ad047b4de44594cb9ae3cffb5095cc28ed4b5
Ingenious <76046349+Ingenious-c0der@users.noreply.github.com>
2021-09-14 20:23:15 +0530

    Update oop_2.cpp

--- oop_2.cpp
+++ oop_2.cpp
@@ -1,7 +1,11 @@
 
 #include <iostream>
 #include <string.h>
+#include<bits/stdc++.h>
 using namespace std;
+
+
+string blood_group[8] = {"A+","A-","O+","O-","AB-","AB+","B+","B-"};
 class Student{
 
 	char* Name;
@@ -13,7 +17,7 @@ class Student{
 	char* address;
 	long long int tele_no;
 	int license_no;
-
+	static int count;
 	public:
 	//Constructor Definitions
 	Student();//Default
@@ -21,17 +25,33 @@ class Student{
 	Student (Student &);//Copy
 	~Student(); //Destructor
 
-
+	friend class Result;
 	//Overloaded insertion and extraction operators
 	friend istream& operator >> (istream& input, Student &S);
 	friend ostream& operator << (ostream& output,Student &S);
 	void get_details();
+	
+	static void get_count(int);
 
 };
+class Result{
+	int marks;
+	public:
+	void calculate_percentage(Student &);
+};
+void Result::calculate_percentage(Student &S){
+		cout<<"Enter marks of student: ";
+		cin>>marks;
+		int percent = (marks*100)/100;
+		cout << "Roll No. " << S.roll_no << " has scored percentage: " << percent <<" %" <<endl;
+
+	}
 
 //Parameterized Constructor
 Student::Student(  char* Name,  char* roll_no, char* Cls, char* div,
 char* dob, char* blood_grp, char*  address,long long int tele_no,int license_no){
+	cout<<"Parameterized Constructor called"<<endl;
+	get_count(1);
 	int length;
 		length = strlen(Name);
 		this->Name = new char[length];
@@ -67,6 +87,8 @@ char* dob, char* blood_grp, char*  address,long long int tele_no,int license_no)
 }
 //Default Constructor
 Student::Student(){
+	cout<<"Default Constructor called"<<endl;
+	get_count(1);
 	Name = new char;
 	roll_no = new char;
 	Cls =new char;
@@ -80,6 +102,8 @@ Student::Student(){
 }
 //Copy Constructor
 Student::Student(Student &S){
+	cout<<"Copy constructor called"<<endl;
+	get_count(1);
 	Name = S.Name;
 	roll_no = S.roll_no;
 	Cls = S.Cls;
@@ -92,6 +116,8 @@ Student::Student(Student &S){
 }
 //Destructor
 Student::~Student(){
+	cout<<"Destructor Called"<<endl;
+	get_count(-1);
 	delete Name;
 	delete roll_no;
 	delete Cls;
@@ -121,7 +147,20 @@ istream& operator >> (istream& input, Student &S ){
 		cout<<"Enter the address of the student : ";
 		cin>>S.address;
 		cout<< "Enter the blood group of the student :";
-		cin>>S.blood_grp;
+		try{
+				cin>>S.blood_grp;
+				if (std::find(begin(blood_group), end(blood_group), S.blood_grp) != end(blood_group))
+				{
+					cout<<"Blood group found"<<endl;
+				}
+				else{
+					throw S.blood_grp;
+				}
+
+		}catch(char* new_blood_grp){
+			cout<<"Blood grp is invalid! Please enter blood group from (A+, A-, B+, B-, AB+, AB-, O+, O-) :"<<endl;
+			cin>>new_blood_grp;
+		}
 		cout<<"Enter the Date of Birth of the student :";
 		cin>>S.dob;
 		cout<<"Enter the roll_no,Class of the student : ";
@@ -139,7 +178,20 @@ inline void Student::get_details(){
 		cout<<"Enter the address of the student : ";
 		cin>>this->address;
 		cout<< "Enter the blood group of the student :";
-		cin>>this->blood_grp;
+		try{
+				cin>>blood_grp;
+				if (std::find(begin(blood_group), end(blood_group), blood_grp) != end(blood_group))
+				{
+					cout<<"Blood group found"<<endl;
+				}
+				else{
+					throw this->blood_grp;
+				}
+
+		}catch(char* new_blood_grp){
+			cout<<"Blood grp is invalid! Please enter blood group from (A+, A-, B+, B-, AB+, AB-, O+, O-) :"<<endl;
+			cin>>new_blood_grp;
+		}
 		cout<<"Enter the Date of Birth of the student :";
 		cin>>this->dob;
 		cout<<"Enter the roll_no,Class of the student : ";
@@ -148,6 +200,12 @@ inline void Student::get_details(){
 		cin>>this->tele_no>>this->license_no;
 		cout<<"Enter the Division of the student : ";
 		cin>>this->div;
+
+}
+int Student::count = 0;
+void Student::get_count(int c){
+	count+=c;
+	cout<<"The current count of objects is : "<<count<<endl;
 
 }
 
@@ -163,13 +221,29 @@ int main() {
 	char* blood_grp =new char;
 	long long int tele_no;
 	int license_no;
-	
+	Result r1;
+
+	cout<<"***Enter the Details for student 1***"<<endl;
 		cout<<"Enter the Name of the student :";
 		cin>> Name;
 		cout<<"Enter the address of the student : ";
 		cin>>address;
 		cout<< "Enter the blood group of the student :";
-		cin>>blood_grp;
+		try{
+				cin>>blood_grp;
+				if (std::find(begin(blood_group), end(blood_group), blood_grp) != end(blood_group))
+				{
+					cout<<"Blood group found"<<endl;
+				}
+				else{
+					throw blood_grp;
+				}
+
+		}catch(char* new_blood_grp){
+			cout<<"Blood grp is invalid! Please enter blood group from (A+, A-, B+, B-, AB+, AB-, O+, O-) :"<<endl;
+			cin>>new_blood_grp;
+		}
+		
 		cout<<"Enter the Date of Birth of the student :";
 		cin>>dob;
 		cout<<"Enter the roll_no,Class of the student : ";
@@ -178,15 +252,24 @@ int main() {
 		cin>>tele_no>>license_no;
 		cout<<"Enter the Division of the student : ";
 		cin>>div;
+	
 	Student s0(Name,roll_no,Cls,div,dob,blood_grp,address,tele_no,license_no);
+	cout<<"************** Enter the details for student 2 **************"<<endl;
 	Student s1;
 	cin>>s1;
+	cout<<"*********** Student 3 is given the same details as student 2 ************"<<endl;
 	Student s2=s1;
 	Student s3;
+	cout<<"********* Enter the details for student 4 ************"<<endl;
 	s3.get_details();
 	cout<<s0;
 	cout<<s1;
 	cout<<s2;
 	cout<<s3;
+	r1.calculate_percentage(s0);
+	r1.calculate_percentage(s1);
+	r1.calculate_percentage(s2);
+	r1.calculate_percentage(s3);
+
 	return 0;
 }
