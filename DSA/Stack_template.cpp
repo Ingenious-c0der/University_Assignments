f72c2ee2c932ce61c43e1a9daa8a2bcfd024aff8
Ingenious <76046349+Ingenious-c0der@users.noreply.github.com>
2021-10-24 18:55:25 +0530

    Update Stack_template.cpp

--- DSA/Stack_template.cpp
+++ DSA/Stack_template.cpp
@@ -29,41 +29,39 @@ void Stack<T,size>::pop(){
 }
 template<typename T , int size>
 void Stack<T,size>::push(T const& val){
-     {
-		if (top < size-1)
+	if (top < size-1)
         {
-			top++;
-			stack[top] = val;
-		}
-		else 
-        {
-			cout << "stack overflow" << endl;
-		}
+		top++;
+		stack[top] = val;
 	}
+	else 
+	{
+		cout << "stack overflow" << endl;
+	}
+	
 }
 template<typename T , int size>
-Stack<T,size>::Stack(){
-    {
-		stack = new T[size];
-	}
-    
+Stack<T,size>::Stack()
+{
+    	stack = new T[size];
 }
 template<typename T , int size>
 void Stack<T,size>::show() {
-		for (int i = 0; i < top+1; i++) {
-			cout << stack[i] <<" ";
+	for (int i = 0; i < top+1; i++)
+		{
+		    cout << stack[i] <<" ";
 		}
         cout<<endl;
-	}
+}
 
 
 int main() {
-	Stack<string,3> s1;
-	s1.push("str1");
-	s1.push("str2");
+   Stack<string,3> s1;
+    s1.push("str1");
+    s1.push("str2");
     s1.push("str3");
     s1.push("str4");
-	s1.show();
+    s1.show();
 
     Stack<char,3> c1;
     c1.push('c');
