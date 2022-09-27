d71eb6014463e2cf2624ab7604a67781127a7610
INGENIOUS <sagarabhyankar18@gmail.com>
2022-09-27 20:51:55 +0530

    -f

--- Comp_Prog/equality_equations.cpp
+++ Comp_Prog/equality_equations.cpp
@@ -34,7 +34,7 @@ public:
             }
             else
             {
-                int pool_1 = componentPool(equation[1]) ; 
+                int pool_1 = componentPool(equation[0]) ; 
                 int pool_2 = componentPool(equation[3]) ; 
                 if (pool_1!=-1 && pool_2!=-1 && pool_1 == pool_2)
                 {
@@ -42,7 +42,7 @@ public:
                 }
                 else if(pool_1 == -1 && pool_2!=-1)
                 {
-                    components[pool_2].push_back(equation[1]); 
+                    components[pool_2].push_back(equation[0]); 
                 }
                 else if(pool_1 != -1 && pool_2 == -1)
                 {
@@ -51,17 +51,19 @@ public:
                 else if(pool_1!=-1 && pool_2!=-1 && pool_1!=pool_2)
                 {
                     vector<char> pool_first = components[pool_1]; 
+                   
                     vector<char> pool_second = components[pool_2] ; 
                     std::set<char> new_set ; 
                     for(auto x: pool_first)
                     {
                         new_set.insert(x);
                     }
-                    for(auto x: pool_first)
+                    for(auto x: pool_second)
                     {
                         new_set.insert(x);
                     }
                     vector<char> new_pool(new_set.begin(),new_set.end()); 
+                    
                     components[pool_1] = {} ; 
                     components[pool_2] = {} ; 
                     components.push_back(new_pool);
@@ -78,25 +80,21 @@ public:
                 
             }
         }
+        
         for(auto vec : unequal)
         {
+            std::cout<<vec[0]<<"-"<<vec[1]<<endl;
            int pool_11 = componentPool(vec[0]);
            int pool_22 = componentPool(vec[1]);
            std::cout<<pool_11<<" "<<pool_22<<endl;
-           
-            if(pool_11==pool_22)
+
+            if(pool_11==pool_22 && pool_11!= -1 )
             {
+
                 return false; 
             }
         }
-        for(auto vec : components)
-        {
-            for(auto x: vec)
-            {
-                cout<<x<<" ";
-            }
-            cout<<endl;
-        }
+        
         return true; 
     }
 };
@@ -104,7 +102,7 @@ public:
 
 int main()
 {
-    vector<string> equations = {"a==b","b!=c","c==a"} ;
-    Solution().equationsPossible(equations) ;
+    vector<string> equations = {"a==b","e==c","b==c","a!=e"} ;
+    std::cout<<Solution().equationsPossible(equations) ;
     return 0;
 }
