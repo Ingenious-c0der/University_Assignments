02d8e872736e383e56edd69e383e2b3f2122e74c
INGENIOUS <sagarabhyankar18@gmail.com>
2023-02-23 20:53:04 +0530

    -fixes

--- LP-2/a_star.cpp
+++ LP-2/a_star.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <cmath>
 class Co_od
 {
@@ -44,51 +45,65 @@ public:
         this->g = g;
         this->f = f;
     }
+    Node(const Node& other)
+    {
+        this->parent = other.parent;
+        this->position = other.position;
+        this->h = other.h;
+        this->g = other.g;
+        this->f = other.f;
+    }
 
     void a_star(std::vector<std::vector<int>> matrix, Co_od start_loc, Co_od end_loc)
     {
-        std::vector<Node> open_list;
-        std::vector<Node> closed_list;
-        Node start_node = Node(nullptr, start_loc, 0, 0, 0);
-        Node end_node = Node(nullptr, end_loc, 0, 0, 0);
+        std::vector<Node*> open_list;
+        std::vector<Node*> closed_list;
+        Node* start_node = new Node(nullptr, start_loc, 0, 0, 0);
+        Node* end_node = new Node(nullptr, end_loc, 0, 0, 0);
         open_list.push_back(start_node);
 
         while (open_list.size() > 0)
         {
-            Node current_node = open_list[0];
+            Node* current_node = open_list[0];
             int current_index = 0;
+           
             for (int i = 0; i < open_list.size(); i++)
             {
-                if (open_list[i].f < current_node.f)
+                
+                if (open_list[i]->f < current_node->f)
                 {
                     current_node = open_list[i];
                     current_index = i;
                 }
             }
+           
             open_list.erase(open_list.begin() + current_index);
             closed_list.push_back(current_node);
-            if (current_node.position == end_node.position)
+            if (current_node->position == end_node->position)
             {
+
                 std::vector<Co_od> path;
-                Node *current = &current_node;
+                Node *current = current_node;
                 while (current)
                 {
                     path.push_back(current->position);
                     current = current->parent;
                 }
+                std::reverse(path.begin(), path.end());
                 // print the path here and return
                 for (auto co_od : path)
                 {
-                    std::cout << co_od.x << " " << co_od.y << std::endl;
+                    std::cout <<"("<< co_od.x << " " << co_od.y <<"),";
                 }
+                std::cout << std::endl;
                 return;
             }
-            std::vector<Node> children;
+           
+            std::vector<Node*> children;
             std::vector<Co_od> explore_delta_directions = {Co_od(0, -1), Co_od(0, 1), Co_od(-1, 0), Co_od(1, 0)};
-            std::cout << "current node " << current_node.position.x << " " << current_node.position.y << std::endl;
             for (auto co_od : explore_delta_directions)
             {
-                Co_od node_position = current_node.position + co_od;
+                Co_od node_position = current_node->position + co_od;
                 if (node_position.x > (matrix.size() - 1) || node_position.x < 0 || node_position.y > (matrix[0].size() - 1) || node_position.y < 0)
                 {
                     continue;
@@ -97,30 +112,32 @@ public:
                 {
                     continue;
                 }
-                Node new_node = Node(&current_node, node_position, 0, 0, 0);
+                Node* new_node = new Node(current_node, node_position, 0, 0, 0);
                 children.push_back(new_node);
             }
+           
             for (auto child : children)
             {
+             
                 for (auto closed_child : closed_list)
                 {
-                    if (child.position == closed_child.position)
+                    if (child->position == closed_child->position)
                     {
                         continue;
                     }
                 }
-                child.g = current_node.g + 1;
-                child.h = int(std::pow(std::pow(abs(child.position.x - end_node.position.x), 2) + std::pow(abs(child.position.y - end_node.position.y), 2), 0.5));
-                child.f = child.g + child.h;
-                std::cout << "child " << child.position.x << " " << child.position.y << std::endl;
+                child->g = current_node->g + 1;
+                child->h = std::pow(abs(child->position.x - end_node->position.x),2) +std::pow( abs(child->position.y - end_node->position.y),2);
+                child->f = child->g + child->h;
                 for (auto open_node : open_list)
                 {
-                    if (child.position == open_node.position && child.g > open_node.g)
+                    if (child->position == open_node->position && child->g > open_node->g)
                     {
                         continue;
                     }
                 }
                 open_list.push_back(child);
+              
             }
         }
     }
@@ -128,7 +145,6 @@ public:
 
 int main()
 {
-    // 4 by 4 matrix with obstacles
     std::vector<std::vector<int>> mat = {
         {0, 1, 0, 0, 0, 0},
         {0, 0, 1, 0, 1, 0},
