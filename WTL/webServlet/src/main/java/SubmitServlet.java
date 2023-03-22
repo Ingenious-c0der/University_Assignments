636b2dfeb45872da3c4a0bec5f5edf133426b335
Sagar Abhyankar <76046349+Ingenious-c0der@users.noreply.github.com>
2023-03-22 14:40:01 +0530

    Add files via upload

---
+++ WTL/webServlet/src/main/java/SubmitServlet.java
@@ -0,0 +1,91 @@
+
+import java.sql.*; 
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.*;
+/**
+ * Servlet implementation class SubmitServlet
+ */
+public class SubmitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection con; 
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SubmitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		String username = request.getParameter("t1");
+		String sqlQuery = "select * from tomcat_user_table where username = \""+username+"\"";
+		String url = "jdbc:mysql://localhost:3306/db";
+		String admin_username = "root";
+		String password = "***********";
+		try {
+			String dbDriver = "com.mysql.cj.jdbc.Driver";
+			Class.forName(dbDriver);
+			this.con = DriverManager.getConnection(url,admin_username,password);
+			PreparedStatement ps = this.con.prepareStatement(sqlQuery);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next())
+			{
+				String password_given = request.getParameter("p1"); 
+				String password_actual = rs.getString(3);
+
+				if(password_given.equals(password_actual))
+				{
+					pw.println("<h1>Welcome User "+request.getParameter("t1")+" logged in successfully!</h1>") ;
+				}
+				else {
+					pw.println("<h1> Incorrect Password Provided </h1>");
+				}
+			}
+			else
+			{
+				String s = request.getParameter("t1");
+				pw.println("<h1>User with user name "+ s +" not found</h1>");
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			pw.println("Error in SQL , CLASS ");
+			e.printStackTrace();
+		} 
+		
+		pw.close();
+	}
+
+}
